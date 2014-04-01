4ff7d4244b32f074a65c3f4b4241466090861253
Ilyas Ronef <ilyas@divandesign.biz>
2014-04-01 16:49:33 +0600

    Version 1.1: * Библиотека «$.url» больше не используется, url разбирается по регулярке. - Удалено поле «$.ddTools.host». + Добавлено поле «$.ddTools.current», содержащее текущий разобранный адрес.

--- jquery.ddUrl.js
+++ jquery.ddUrl.js
@@ -1,11 +1,10 @@
 /**
  * jQuery ddUrl Plugin
- * @version 1.0b (2012-06-18)
+ * @version 1.1 (2012-09-05)
  * 
  * @desc Библиотека для работы с URL.
  * 
  * @uses jQuery 1.7.2
- * @uses $.url 2.0
  * 
  * @copyright 2012, DivanDesign
  * http://www.DivanDesign.biz
@@ -13,48 +12,61 @@
 
 (function($){
 $.ddUrl = {
-	host: $.url().attr('host'),
+	current: {},
 	
 	get: function(url){
-		var _this = this;
-		
 		//Если url не передан
 		if ($.type(url) != 'string'){
 			//Берём за основу текущий урл
-			url = $.url();
-		}else{
-			//Пытаемся получить урл
-			url = $.url(url);
+			return this.current;
 		}
 		
-		var result = {
-			host: url.attr('host'),
-			url: url.attr('relative'),
-			full: '',
-			internal: false
-		};
+		var regResult = /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*):?([^:@]*))?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/.exec(decodeURI(url)),
+			result = {
+				//Полный адрес (обработаем позже)
+				full: '',
+				//Протокол
+				protocol: regResult[1] || window.location.protocol.replace(':', ''),
+				//Хост
+				host: regResult[6] || '',
+				//Порт
+				port: regResult[7] || '',
+				//Относительный адрес (без протокола, хоста и порта)
+				relative: regResult[8],
+				//Путь (без протокола, хоста, порта и всех параметров)
+				path: regResult[9],
+				//Get-Параметры
+				query: regResult[12] || '',
+				//Хэш
+				hash: regResult[13] || '',
+				//Внутренняя ли это ссылка
+				internal: false
+			};
 		
 		//Если хост не пустой
 		if (result.host != ''){
 			//Но при этом инвалидный (не содержит доменное окончание) (относительная ссылка без «/» в начале)
 			if (result.host.search(/\.[^\.]+/) == -1){
 				//Добавим несчастному слэш в начале
-				result.url = '/' + result.host + result.url;
+				result.relative = '/' + result.host + result.relative;
 				//Сбросим хост, ибо это внутренняя ссылка
-				result.host = _this.host;
+				result.host = window.location.hostname;
 			}
 		}else{
 			//Выставим текущий хост, ибо это внутренняя ссылка
-			result.host = _this.host;
+			result.host = window.location.hostname;
 		}
 		
 		//Если мы находимся в текущем домене, то это внутренняя ссылка
-		if (result.host == _this.host) result.internal = true;
+		if (result.host == window.location.hostname) result.internal = true;
 		
-//		result.url = $.url(result.url).attr('relative');
-		result.full = (url.attr('protocol') || 'http') + '://' + result.host + result.url;
+		//Полный урл
+		result.full = result.protocol + '://' + result.host + (result.port != '' ? ':' + result.port : '') + result.relative;
 		
 		return result;
 	}
 };
+
+//Распарсим один раз текущий url
+$.ddUrl.current = $.ddUrl.get(window.location.toString());
 })(jQuery);
