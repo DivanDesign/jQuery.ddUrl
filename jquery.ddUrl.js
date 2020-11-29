b328c5b9db30ce100f8728232d909c89f4acefcc
Ilyas Ronef <ilyas@DivanDesign.ru>
2020-11-29 17:37:24 +0300

    * Minor changes.

--- jquery.ddUrl.js
+++ jquery.ddUrl.js
@@ -1,13 +1,12 @@
 /**
- * jQuery ddUrl Plugin
+ * jQuery.ddUrl
  * @version 1.5 (2015-07-27)
  * 
  * @desc Библиотека для работы с URL.
  * 
  * @requires jQuery 1.7.2
  * 
- * @copyright 2015, DivanDesign
- * http://www.DivanDesign.biz
+ * @copyright 2012–2015 [DD Group]{@link https://DivanDesign.biz }
  */
 
 (function($){
@@ -16,25 +15,29 @@ $.ddUrl = {
 	
 	/**
 	 * @method parseQuery
-	 * @version 1.2 (2015-04-26)
+	 * @version 1.2.1 (2020-11-29)
 	 * 
 	 * @desc Разбивает строку запроса в объект. Поддерживаются также конструкции вида «param[key][]=1».
 	 * 
 	 * @param query {string} — Строка запроса.
 	 * 
-	 * @returns {plain object}
+	 * @returns {objectPlain}
 	 */
 	parseQuery: function(query){
 		/**
 		 * @desc Устанавливает значение необходимому элементу объекта с проверкой на массив.
 		 * 
-		 * @param obj {plain object} — Исходный объект.
+		 * @param obj {objectPlain} — Исходный объект.
 		 * @param key {string} — Ключ.
 		 * @param value {mixed} — Значение.
 		 * 
 		 * @returns {void}
 		 */
-		function setValue(obj, key, value){
+		function setValue(
+			obj,
+			key,
+			value
+		){
 			//Если элемент существует и является массивом
 			if ($.isArray(obj[key])){
 				//Докидываем значение
@@ -50,15 +53,21 @@ $.ddUrl = {
 		 * 
 		 * @param arr {array} — Исходный массив.
 		 * 
-		 * @returns {plain object}
+		 * @returns {objectPlain}
 		 */
 		function arrayToObject(arr){
 			var result = {};
 			
 			//Перебираем элементы элемента и закидываем в объект
-			$.each(arr, function(key, val){
-				result[key] = val;
-			});
+			$.each(
+				arr,
+				function(
+					key,
+					val
+				){
+					result[key] = val;
+				}
+			);
 			
 			return result;
 		}
@@ -67,67 +76,114 @@ $.ddUrl = {
 		 * @desc Рекурсивно разбирает «сложный» ключ и присваивает значение необходимому элементу объекта.
 		 * 
 		 * @param keyParts {array} — Части ключа (ключ, разбитый по '[').
-		 * @param obj {plain object} — Самый главный результирующий объект.
+		 * @param obj {objectPlain} — Самый главный результирующий объект.
 		 * @param key {string} — Ключ элемента, которому необходимо присвоить значение.
 		 * @param value {mixed} — Значение.
 		 * 
 		 * @returns {void}
 		 */
-		function parseQueryItem(keyParts, obj, key, value){
+		function parseQueryItem(
+			keyParts,
+			obj,
+			key,
+			value
+		){
 			//Самый первый возможный ключ (помним, что из массива он вырезается)
 			var keyParts_first = keyParts.shift();
 			
 			//Если сегментов ключа больше нет (значит всё уже разобрали, осталось только присвоить значение)
 			if (!keyParts_first){
 				//Присваиваем
-				setValue(obj, key, value);
+				setValue(
+					obj,
+					key,
+					value
+				);
 			}else{
 				//Если такого элемента нет, определим по дефолту как массив
-				if (!obj[key]){obj[key] = [];}
+				if (!obj[key]){
+					obj[key] = [];
+				}
 				
 				//Если ключ пустой (был равен «[]», но после разбиения осталась только закрывающая скобка)
 				if (keyParts_first == ']'){
 					//Присваиваем значение
-					setValue(obj, key, value);
+					setValue(
+						obj,
+						key,
+						value
+					);
 				//Самый обычный ключ
 				}else{
 					//Если в ключе есть закрывающая скобка (в самом конце)
 					if (keyParts_first.indexOf(']') != -1){
 						//Обрезаем её (последний символ)
-						keyParts_first = keyParts_first.substr(0, keyParts_first.length - 1);
+						keyParts_first = keyParts_first.substr(
+							0,
+							keyParts_first.length - 1
+						);
 					}
 					
 					//Ключ не числовой, но элемент является массивом, надо преобразовать массив в объект с числовыми ключами
-					if (!$.isNumeric(keyParts_first) && $.isArray(obj[key])){
+					if (
+						!$.isNumeric(keyParts_first) &&
+						$.isArray(obj[key])
+					){
 						//Преобразовываем массив в объект
 						obj[key] = arrayToObject(obj[key]);
 					}
 					
 					//Помним, что keyParts уже не содержит первого элемента
-					parseQueryItem(keyParts, obj[key], keyParts_first, value);
+					parseQueryItem(
+						keyParts,
+						obj[key],
+						keyParts_first,
+						value
+					);
 				}
 			}
 		}
 		
-		//Дополнительная обёртка необходима для удобства при рекурсивном разборе «сложного» ключа
-		var result = {result: {}};
+		var
+			//Дополнительная обёртка необходима для удобства при рекурсивном разборе «сложного» ключа
+			result = {
+				result: {}
+			}
+		;
 		
 		//Если что-то вообще передали
 		if (query.length > 0){
 			//Разбиваем по паре ключ-значение
 			query = query.split('&');
 			
-			for (var i = 0; i < query.length; i++){
-				var elem = query[i].split('='),
+			for (
+				var i = 0;
+				i < query.length;
+				i++
+			){
+				var
+					elem = query[i].split('='),
 					key = elem[0],
-					value = elem[1] || '';
+					value =
+						elem[1] ||
+						''
+				;
 				
 				//Если это обычный ключ
 				if (key.indexOf('[') == -1){
 					//Просто установим значение
-					setValue(result.result, key, value);
+					setValue(
+						result.result,
+						key,
+						value
+					);
 				}else{
-					parseQueryItem(key.split('['), result, 'result', value);
+					parseQueryItem(
+						key.split('['),
+						result,
+						'result',
+						value
+					);
 				}
 			}
 		}
@@ -137,19 +193,25 @@ $.ddUrl = {
 	
 	/**
 	 * @method parseHash
-	 * @version 0.1 (2017-01-30)
-	 *
+	 * @version 0.1.1 (2020-11-29)
+	 * 
 	 * @desc Разбивает строку хеша на вложенности в пути.
-	 *
+	 * 
 	 * @param hash {string} — Строка хэша.
-	 *
+	 * 
 	 * @returns {array}
 	 */
 	parseHash: function(hash){
-		var hashPath_array = hash.split('/'),
-			hashPath = [];
+		var
+			hashPath_array = hash.split('/'),
+			hashPath = []
+		;
 		
-		for (var i = 0; i < hashPath_array.length; i++){
+		for (
+			var i = 0;
+			i < hashPath_array.length;
+			i++
+		){
 			var elem = $.trim(hashPath_array[i]);
 			
 			if(elem.length != 0){
@@ -157,18 +219,19 @@ $.ddUrl = {
 				hashPath.push(elem);
 			}
 		}
+		
 		return hashPath;
 	},
 	
 	/**
 	 * @method parse
-	 * @version 1.2.1 (2015-07-23)
+	 * @version 1.2.2 (2020-11-29)
 	 * 
 	 * @desc Разбивает строку url в объект.
 	 * 
 	 * @param [url = window.location.toString()] {string} — Строка url.
 	 * 
-	 * @returns {plain object}
+	 * @returns {objectPlain}
 	 */
 	parse: function(url){
 		var _this = this;
@@ -179,7 +242,8 @@ $.ddUrl = {
 			url = window.location.toString();
 		}
 		
-		var regResult = /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*):?([^:@]*))?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/.exec(decodeURI(url)),
+		var
+			regResult = /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*):?([^:@]*))?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/.exec(decodeURI(url)),
 			result = {
 				//Полный адрес (обработаем позже)
 				full: '',
@@ -205,7 +269,8 @@ $.ddUrl = {
 				hashString: regResult[13] || '',
 				//Внутренняя ли это ссылка
 				internal: false
-			};
+			}
+		;
 		
 		//Если хэш не пустой
 		if (result.hashString.length > 0){
@@ -223,7 +288,11 @@ $.ddUrl = {
 			//Но при этом инвалидный (не содержит доменное окончание) (относительная ссылка без «/» в начале)
 			if (result.host.search(/\.[^\.]+/) == -1){
 				//Добавим несчастному слэш в начале
-				result.relative = '/' + result.host + result.relative;
+				result.relative =
+					'/' +
+					result.host +
+					result.relative
+				;
 				//Сбросим хост, ибо это внутренняя ссылка
 				result.host = window.location.hostname;
 			}
@@ -233,10 +302,25 @@ $.ddUrl = {
 		}
 		
 		//Если мы находимся в текущем домене, то это внутренняя ссылка
-		if (result.host == window.location.hostname){result.internal = true;}
+		if (result.host == window.location.hostname){
+			result.internal = true;
+		}
 		
 		//Полный урл
-		result.full = result.protocol + '://' + result.host + (result.port != '' ? ':' + result.port : '') + result.relative;
+		result.full =
+			result.protocol +
+			'://' +
+			result.host +
+			(
+				result.port != '' ?
+				(
+					':' +
+					result.port
+				) :
+				''
+			) +
+			result.relative
+		;
 		
 		return result;
 	}
@@ -248,9 +332,18 @@ $.ddUrl.current = $.ddUrl.parse();
 //On document.ready
 $(function(){
 	//При изменении url (кнопки «Назад» и пр.)
-	(($.ddTools && $.ddTools.$window) || $(window)).on('popstate', function(event){
-		//Распарсим текущий url
-		$.ddUrl.current = $.ddUrl.parse();
-	});
+	(
+		(
+			$.ddTools &&
+			$.ddTools.$window
+		) ||
+		$(window)
+	).on(
+		'popstate',
+		function(event){
+			//Распарсим текущий url
+			$.ddUrl.current = $.ddUrl.parse();
+		}
+	);
 });
 })(jQuery);
